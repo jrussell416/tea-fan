fe6f6f07afc2883192d25cef11d71d10e374009e
JENNIFER RUSSELL <JC.RUSSELL@yahoo.com>
2021-06-05 16:40:19 -0400

    added project

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,20 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  root to: "welcome#index"
+  resources :signups
+  resources :dashboards
+    
+  delete "logout", to: "dashboards#destroy"
+  resources :logins
+
+  resources :recipes do
+    put :favorite, on: :member
+  end
+
+  resources :favorite_recipes
+  
+  resources :shops 
+  
+  resources :stashes
+
 end
